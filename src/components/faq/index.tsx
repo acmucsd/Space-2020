81948885c68cc63ecf9542e7d3e4ae802842a5ab
annsudhart <asudhart@ucsd.edu>
2020-01-02 00:00:51 -0800

    added base of faq component

--- src/components/faq/index.tsx
+++ src/components/faq/index.tsx
@@ -4,7 +4,32 @@ import './style.less';
 const FAQ: React.FC = () => {
   return (
     <div className="faq">
-      <p>Hello World!</p>
+      <div className="faq-header">
+        <h1>FAQs</h1>
+      </div>
+      <div className="faq-body">
+        <div className="break"></div>
+        <button className="accordion">Where can I find more info?<div className="symbol"><img src="/arrow.png"/></div></button>
+        <div className="panel">
+          <p>Content</p>
+        </div>
+        <div className="break"></div>
+        <button className="accordion">Should I bring my resume?<div className="symbol"><img src="/arrow.png"/></div></button>
+        <div className="panel">
+          <p>We will be collecting resumes for distribution digitally, but some companies may accept physical copies!</p>
+        </div>
+        <div className="break"></div>
+        <button className="accordion">Will there be free swag?<div className="symbol"><img src="/arrow.png"/></div></button>
+        <div className="panel">
+          <p>Content</p>
+        </div>
+        <div className="break"></div>
+        <button className="accordion">Will the event be on Handshake?<div className="symbol"><img src="/arrow.png"/></div></button>
+        <div className="panel">
+          <p>Content</p>
+        </div>
+        <div className="break"></div>
+      </div>
     </div>
   );
 }
