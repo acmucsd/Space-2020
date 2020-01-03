4ec1cc5334a3afbedc196b86539776f4ab78c406
annsudhart <asudhart@ucsd.edu>
2020-01-03 07:46:45 -0800

    made mobile version sans clickable menu icon

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,13 +1,20 @@
 import React, { useState, useEffect } from "react";
-
 import "./style.less";
+import rocket from "../../assets/mini-rocket.svg";
+import hamburger from "../../assets/menu-icon.svg";
 
 const Navbar: React.FC = () => {
 
   return (
     <nav className="navbar navbar-expand">
-        <a href="#" className="navbar-brand navbar-item"></a>
-        
+        <a href="#" className="navbar-brand navbar-item">
+            <img src={rocket} alt="SPACE-rocket-brand"/>
+        </a>
+
+        <div className="navbar-item menu-icon">
+            <img src={hamburger}/>
+        </div>
+
         <ul>
             <li className="navbar-list navbar-item"><a href="#">About</a></li>
             <li className="navbar-list navbar-item"><a href="#">Sponsors</a></li>
