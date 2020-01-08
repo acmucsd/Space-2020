2e83d22cb9f41fbb04e7eaef24f758617460a552
Paul Pan <panjunhong05@gmail.com>
2020-01-07 18:32:52 -0800

    Added footer

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import Page1 from "./pages/page1/";
 import Page2 from "./pages/page2/";
 import Page3 from "./pages/page3/";
 import Page4 from "./pages/page4/";
+import Footer from "./pages/footer/";
 import Particle from "./components/particle-js";
 
 const App: React.FC = () => {
@@ -17,6 +18,7 @@ const App: React.FC = () => {
         <Page2 />
         <Page3 />
         <Page4 />
+        <Footer />
       </div>
     </div>
   );
---
+++ src/pages/footer/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./style.less";
+import email from "../../assets/email.svg";
+import facebook from "../../assets/facebook.svg";
+import medium from "../../assets/medium.svg";
+import space from "../../assets/space.svg";
+
+const Footer: React.FC = () => {
+  return (
+    <div>
+      <img className="astro" src={space} alt="Space" />
+      <div className="links">
+        <a href="mailto:acm@ucsd.edu">
+          <img className="link" src={email} alt="Email" />
+        </a>
+        <a href="https://www.facebook.com/acmucsd/">
+          <img className="link" src={facebook} alt="Facebook" />
+        </a>
+        <a href="https://medium.com/acmucsd">
+          <img className="link" src={medium} alt="Medium" />
+        </a>
+      </div>
+      <p className="footer-font">© UCSD ACM 2019-2020</p>
+    </div>
+  );
+};
+
+export default Footer;
