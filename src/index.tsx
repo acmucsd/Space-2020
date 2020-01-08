86cf0df31a41cebb5af48d06a62095969795f28a
Andrea Sudharta <asudhart@ucsd.edu>
2020-01-07 16:57:02 -0800

    created navbar and page1

--- src/index.tsx
+++ src/index.tsx
@@ -7,11 +7,13 @@ import Page2 from "./pages/page2/";
 import Page3 from "./pages/page3/";
 import Page4 from "./pages/page4/";
 import Particle from "./components/particle-js";
+import Navbar from "./components/navbar";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <Particle />
+      <Navbar/>
       <div className="pages">
         <Page1 />
         <Page2 />
